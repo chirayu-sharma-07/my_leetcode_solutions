2b5568230ad4e71686e4f0d5f106ac93afc46da9
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2023-03-01 22:38:53 +0530

    Time: 35 ms (57.30%), Space: 53.5 MB (45.27%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,6 +1,40 @@
 class Solution {
+    public static void conquer(int[] nums,int e,int mid, int f){
+        int[] myArray=new int[(f-e)+1];
+        int i=e;
+        int j=mid+1;
+        int k=0;
+        while(i<=mid && j<=f){
+            if(nums[i]<nums[j]){
+                myArray[k]=nums[i];
+                i++;
+            }else{
+                myArray[k]=nums[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<=mid){
+            myArray[k]=nums[i];
+            i++;
+            k++;
+        }
+        while(j<=f){
+            myArray[k]=nums[j];
+            k++;
+            j++;
+        }
+        for(int x=0;x<myArray.length;x++,e++) nums[e]=myArray[x];
+    }
+    public static void divide(int[] nums,int e,int f){
+        if(e>=f) return;
+        int mid=e+((f-e)/2);
+        divide(nums,e,mid);
+        divide(nums,mid+1,f);
+        conquer(nums,e,mid,f);
+    }
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+        divide(nums,0,nums.length-1);
         return nums;
     }
 }
