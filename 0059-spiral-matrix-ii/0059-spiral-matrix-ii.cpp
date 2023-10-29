311a4013c31cb76067be6049e90688fea74b9741
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2023-10-29 22:27:04 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (34.20%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        int top=0;
-        int bottom=n-1;
-        int left=0;
-        int right=bottom;
         vector<vector<int>> result;
         result.resize(n);
         for(int e=0;e<n;e++){
             result[e].reserve(n);
         }
         int matrix[n][n];
-        for(int e=0;e<n;e++){
-            for(int f=0;f<n;f++){
-                matrix[e][f]=0;
-            }
-        }
-        for(int e=1;top<=bottom && left<=right;){
-            for(int f=left;f<=right;f++){
-                matrix[top][f]=e;
-                e++;
+        int top=0;
+        int bottom=n-1;
+        int left=0;
+        int right=bottom;
+        int num=1;
+        while(top<=bottom && left<=right){
+            for(int e=left;e<=right;e++){
+                matrix[top][e]=num;
+                num++;
             }
             top++;
-            for(int f=top;f<=bottom;f++){
-                matrix[f][right]=e;
-                e++;
+            for(int e=top;e<=bottom;e++){
+                matrix[e][right]=num;
+                num++;
             }
             right--;
-            for(int f=right;f>=left;f--){
-                matrix[bottom][f]=e;
-                e++;
+            for(int e=right;e>=left;e--){
+                matrix[bottom][e]=num;
+                num++;
             }
             bottom--;
-            for(int f=bottom;f>=top;f--){
-                matrix[f][left]=e;
-                e++;
+            for(int e=bottom;e>=top;e--){
+                matrix[e][left]=num;
+                num++;
             }
             left++;
         }
