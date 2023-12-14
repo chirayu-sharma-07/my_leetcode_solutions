e2ca11630c319ffb4caeb6d9e533e5877d111a9f
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2023-12-14 19:51:40 +0530

    Time: 13 ms (83.20%), Space: 13.3 MB (25.87%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,28 +1,22 @@
 class Solution {
-private:
-    bool myFunction(vector<vector<int>> &mat,int e,int f,int m,int n){
-        for(int i=0;i<m;i++){
-            if(mat[i][f]!=0){
-                if(i!=e) return false;
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(mat[e][i]!=0){
-                if(f!=i) return false;
-            }
-        }
-        return true;
-    }
 public:
     int numSpecial(vector<vector<int>>& mat) {
-        int result=0;
         int m=mat.size();
         int n=mat[0].size();
+        vector<int> first(m,0);
+        vector<int> second(n,0);
         for(int e=0;e<m;e++){
             for(int f=0;f<n;f++){
                 if(mat[e][f]==1){
-                    if(myFunction(mat,e,f,m,n)) result++;
+                    first[e]++;
+                    second[f]++;
                 }
+            }
+        }
+        int result=0;
+        for(int e=0;e<m;e++){
+            for(int f=0;f<n;f++){
+                if(mat[e][f]==1 && first[e]==1 && second[f]==1) result++;
             }
         }
         return result;
