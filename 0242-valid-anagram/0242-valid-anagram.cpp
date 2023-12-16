e76a1fe5a3035ea7b1c752d42fe89b4bb75e5db7
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2023-12-16 12:36:22 +0530

    Time: 15 ms (19.45%), Space: 7.6 MB (79.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        //First approach by sam_chirayu
+        // TC - 98.77% acceptance
+        // SC - 79.21% acceptance
+        // Both are more than my 12th percentage ;)
+        /*
         if(s.size()!=t.size()) return false;
         vector<int> arr(26,0);
         int n=s.size();
@@ -12,6 +17,17 @@ public:
         }
         for(int e=0;e<26;e++){
             if(arr[e]!=0) return false;
+        }
+        return true;
+        */
+        //Second approach by sam_chirayu
+        //This is brute force approach (sorting involved)
+        if(s.size()!=t.size()) return false;
+        sort(begin(s),end(s));
+        sort(t.begin(),t.end());
+        int n=s.size();
+        for(int e=0;e<n;e++){
+            if(s[e]!=t[e]) return false;
         }
         return true;
     }
