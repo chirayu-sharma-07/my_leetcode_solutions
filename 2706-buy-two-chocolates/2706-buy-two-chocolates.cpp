f0e116df646521bfdd2e7f6f9bc5e65fad537516
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2023-12-20 08:38:19 +0530

    Time: 19 ms (19.09%), Space: 47.1 MB (29.77%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,10 +1,33 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
+        //Brute force approach
+        
+        /*
         sort(begin(prices),end(prices));
         int e=prices[0];
         int f=prices[1];
         if(e+f>money) return money;
         return money-(e+f);
+        */
+
+        //Smart approach
+        int smallest=INT_MAX;
+        int secondSmallest=smallest;
+        int n=prices.size();
+        int identifier=-1;
+        for(int e=0;e<n;e++){
+            if(smallest==prices[e]) identifier=smallest;
+            if(prices[e]<smallest) smallest=prices[e];
+        }
+        if(smallest==identifier){
+            if(smallest+smallest>money) return money;
+            return money-(smallest+smallest);
+        }
+        for(int e=0;e<n;e++){
+            if(prices[e]<secondSmallest && prices[e]>smallest) secondSmallest=prices[e];
+        }
+        if(smallest+secondSmallest>money) return money;
+        return money-(smallest+secondSmallest);
     }
 };
