fbb720f21eb46ab9ba7a9034785eec108ee41bbd
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2023-12-24 10:23:30 +0530

    Time: 4 ms (38.33%), Space: 7.2 MB (65.28%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.cpp
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    // First approach
+    // TC :- 100%
+    // SC :- 93.92%
+
+    /*
     struct PairHash {
     template <class T1, class T2>
     size_t operator () (const pair<T1, T2>& p) const {
@@ -8,8 +13,11 @@ public:
         return h1 ^ h2;
         }
     };
+
+    */
     bool isPathCrossing(string path) {
-        unordered_set<pair<int,int>,PairHash> mySet;
+     
+     /*   unordered_set<pair<int,int>,PairHash> mySet;
         int x=0;
         int y=0;
         int n=path.size();
@@ -21,6 +29,30 @@ public:
             else y--;
             if(mySet.find({x,y})!=mySet.end()) return true;
             mySet.insert({x,y});
+        }
+        return false;
+
+        */
+
+        // Second approach
+
+        unordered_set<string> s;
+        string str="";
+        str=to_string(0)+"_"+to_string(0);
+        s.insert(str);
+        int n=path.size();
+        int i=0;
+        int j=0;
+        for(int e=0;e<n;e++){
+            if(path[e]=='N') j++;
+            else if(path[e]=='S') j--;
+            else if(path[e]=='E') i++;
+            else i--;
+            str=to_string(i)+'_'+to_string(j);
+            if(s.find(str)!=s.end()){
+                return true;
+            }
+            s.insert(str);
         }
         return false;
     }
