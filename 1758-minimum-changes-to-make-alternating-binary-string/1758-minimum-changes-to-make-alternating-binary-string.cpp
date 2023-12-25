53919b9949e584d5d499c38efe97d71eb2d0b107
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2023-12-25 11:00:39 +0530

    Time: 6 ms (35.63%), Space: 7.2 MB (98.50%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int minOperations(string s) {
+        // Approach One
+        
+        /*
         int n=s.size();
         int start_with_zero=0;
         int start_with_one=0;
@@ -14,5 +17,19 @@ public:
             }
         }
         return (start_with_zero<start_with_one)?start_with_zero:start_with_one;
+        */
+        
+        // Approach two
+        
+        int n=s.size();
+        int start_with_zero=0;
+        for(int e=0;e<n;e++){
+            if(e%2==0){
+                if(s[e]=='1') start_with_zero++;
+            }else{
+                if(s[e]=='0') start_with_zero++;
+            }
+        }
+        return (start_with_zero<=n/2)?start_with_zero:n-start_with_zero;
     }
 };
