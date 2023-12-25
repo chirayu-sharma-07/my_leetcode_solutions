13d284ab0721146f3f906ff6bd26fa857750c2e0
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2023-12-25 19:05:25 +0530

    Time: 3 ms (47.64%), Space: 6.4 MB (87.76%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int memo[101];
+    int myFunction(int e,int n,string &s){
+        if(memo[e]!=-1) return memo[e];
+        if(e==n) return memo[e]=1;
+        if(s[e]=='0') return 0;
+        int result=0;
+        result+=myFunction(e+1,n,s);
+        if(e+1<n){
+            if(s[e]=='1' || (s[e]=='2' && s[e+1]<='6')){
+                result+=myFunction(e+2,n,s);
+            }
+        }
+        return memo[e]=result;
+    }
+    int numDecodings(string s) {
+        int n=s.size();
+        memset(memo,-1,sizeof(memo));
+        return myFunction(0,n,s);
+    }
+};
