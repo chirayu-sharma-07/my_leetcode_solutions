63e8b4ed9255289cd4788a5ac7900e81bf3c006e
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2023-12-27 17:59:38 +0530

    Time: 3 ms (55.75%), Space: 6.4 MB (47.64%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+
+    //First approach
+    // Recursion+Memoization
+    // TC - O(2^n) very bad solution if memoization is not applied
+    // SC - O(n) for memoization array
+    /*
     int memo[31];
     int myFunction(int n){
         if(memo[n]!=-1) return memo[n];
@@ -10,5 +16,22 @@ public:
         memo[0]=0;
         memo[1]=1;
         return myFunction(n);
+    }
+    */
+
+    //Second Approach
+    // Bottom-up approach
+    // TC - O(n)
+    // SC - O(n)
+
+    int fib(int n){
+        if(n<=1) return n;
+        vector<int> bot_up(n+1,-1);
+        bot_up[0]=0;
+        bot_up[1]=1;
+        for(int e=2;e<=n;e++){
+            bot_up[e]=bot_up[e-1]+bot_up[e-2];
+        }
+        return bot_up[n];
     }
 };
