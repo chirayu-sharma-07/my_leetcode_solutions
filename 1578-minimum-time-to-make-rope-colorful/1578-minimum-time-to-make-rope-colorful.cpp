360ccbf31165296c74b537ad53f0f3457fdab511
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2023-12-27 09:51:03 +0530

    Time: 113 ms (75.86%), Space: 95.7 MB (83.31%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
+
+        // First approach 
+
+        /*
         int n=neededTime.size();
         int result=0;
         for(int e=0;e<n-1;e++){
@@ -17,6 +21,27 @@ public:
                 count+=neededTime[e];
                 pq.push(neededTime[e]);
                 result+=count-pq.top();
+            }
+        }
+        return result;
+        */
+
+        // Second approach
+
+        int n=neededTime.size();
+        int result=0;
+        for(int e=0;e<n-1;e++){
+            if(colors[e]==colors[e+1]){
+                int largest=0;
+                int sum=0;
+                while(e<n-1 && colors[e]==colors[e+1]){
+                    if(largest<neededTime[e]) largest=neededTime[e];
+                    sum+=neededTime[e];
+                    e++;
+                }
+                if(largest<neededTime[e]) largest=neededTime[e];
+                sum+=neededTime[e];
+                result+=sum-largest;
             }
         }
         return result;
