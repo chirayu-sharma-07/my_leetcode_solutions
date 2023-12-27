ae56bce72a34f40b13d99fefd23b87fbd1e444b3
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2023-12-27 17:44:34 +0530

    Time: 2 ms (75.47%), Space: 6.3 MB (47.64%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -3,7 +3,7 @@ public:
     int memo[31];
     int myFunction(int n){
         if(memo[n]!=-1) return memo[n];
-        return myFunction(n-1)+myFunction(n-2);
+        return memo[n]=myFunction(n-1)+myFunction(n-2);
     }
     int fib(int n) {
         memset(memo,-1,sizeof(memo));
