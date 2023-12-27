6efd0305cb927932430ef942cf4720166187774c
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2023-12-27 18:39:01 +0530

    Time: 3 ms (13.76%), Space: 6.6 MB (17.91%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+
+    // Approach One
+    // Recursion_Memoization
+
+    /*
     int myFunction(int n,vector<int> &memo){
         if(n<0) return 0;
         if(n==0) return memo[n]=1;
@@ -11,5 +16,25 @@ public:
     int climbStairs(int n) {
         vector<int> memo(n+1,-1);
         return myFunction(n,memo);
+    }
+    */
+
+    // Second approach
+    // Bottom-up DP
+    // TC - O(n)
+    // SC - O(n)
+    
+    int climbStairs(int n){
+        if(n==1) return 1;
+        if(n==2 || n==3) return (n==2)?2:3;
+        vector<int> bot_up(n+1,-1);
+        bot_up[0]=0;
+        bot_up[1]=1;
+        bot_up[2]=2;
+        bot_up[3]=3;
+        for(int e=4;e<=n;e++){
+            bot_up[e]=bot_up[e-1]+bot_up[e-2];
+        }
+        return bot_up[n];
     }
 };
