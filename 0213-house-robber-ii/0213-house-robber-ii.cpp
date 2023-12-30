3a4e4d4c4cf2484b27cf7d51dce6f81870fb410e
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2023-12-31 01:45:54 +0530

    Time: 6 ms (29.68%), Space: 8.1 MB (86.88%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    
-    // First approach
-    // Recursion + Memoization
-    
     int memo[101];
     int myFunction(int e,int n,vector<int> &nums){
         if(e>=n) return 0;
         if(memo[e]!=-1) return memo[e];
         int steal=nums[e]+myFunction(e+2,n,nums);
         int skip=myFunction(e+1,n,nums);
-        return memo[e]=(steal<skip)?skip:steal;
+        return memo[e]=(steal>skip)?steal:skip;
     }
     int rob(vector<int>& nums) {
-        if(nums.size()==1) return nums[0];
+        int n=nums.size();
+        if(n==1) return nums[0];
+        if(n==2) return (nums[0]<nums[1])?nums[1]:nums[0];
         memset(memo,-1,sizeof(memo));
-        int include_first_house=myFunction(0,nums.size()-1,nums);
+        int include_first_house=myFunction(0,n-1,nums);
         memset(memo,-1,sizeof(memo));
-        int skip_first_house=myFunction(1,nums.size(),nums);
+        int skip_first_house=myFunction(1,n,nums);
         return (include_first_house<skip_first_house)?skip_first_house:include_first_house;
     }
 };
