fa43bfed8ec352b63b1104f9e6725c3e6bf8bcb9
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-01-02 13:29:28 +0530

    Time: 29 ms (5.35%), Space: 29.7 MB (92.71%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
+        // Approach One
+        // Slow code in terms of runtime but optimized in memory terms
+
+        /*
         vector<vector<int>> result;
         vector<int> sub_result;
         int n=nums.size();
@@ -18,6 +22,35 @@ public:
                 }
             }
             if(e==n+1 && sub_result.size()==0) break;
+            result.push_back(sub_result);
+        }
+        return result;
+        */
+
+        // Approach Two
+        // Fast in terms of runtime
+
+        vector<vector<int>> result;
+        vector<int> sub_result;
+        int n=nums.size();
+        int largest=-1;
+        for(int e=0;e<n;e++){
+            if(nums[e]>largest) largest=nums[e];
+        }
+        vector<int> freq(largest+1,0);
+        for(int e=0;e<n;e++){
+            freq[nums[e]]++;
+        }
+        while(true){
+            sub_result.clear();
+            int e=0;
+            for(;e<=largest;e++){
+                if(freq[e]!=0){
+                    sub_result.push_back(e);
+                    freq[e]--;
+                }
+            }
+            if(e==largest+1 && sub_result.size()==0) break;
             result.push_back(sub_result);
         }
         return result;
