87b850108b0233b7a09ee2e6544ef451cf91d4da
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-01-05 20:54:39 +0530

    Time: 527 ms (23.88%), Space: 35.2 MB (26.25%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,10 +1,10 @@
 int memo[2501][2501];
 class Solution {
 public:
-    
-    // Approach One
-    // Recursion + Memoization
-    
+
+    // Approach one
+    // Recursive + Memoization
+
     int n=0;
     int longestIncreasingSubsequence(int curr_index,int prev_index,vector<int>& nums){
         if(curr_index>=n) return 0;
@@ -14,8 +14,8 @@ public:
             take=1+longestIncreasingSubsequence(curr_index+1,curr_index,nums);
         }
         int skip=longestIncreasingSubsequence(curr_index+1,prev_index,nums);
-        if(prev_index==-1) return max(take,skip);
-        return memo[curr_index][prev_index]=max(take,skip);
+        if(prev_index==-1) return (take<skip)?skip:take;
+        return memo[curr_index][prev_index]=(take<skip)?skip:take;
     }
     int lengthOfLIS(vector<int>& nums) {
         n=nums.size();
