a47f2a97d3d1702c192ef3c0edda27216506b584
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-01-11 17:45:20 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (59.95%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -15,6 +15,7 @@ public:
     // Brute force approach
     // Very bad code
     
+    /*
     int result=0;
     void myFunction(TreeNode *root,TreeNode *child){
         result=max(result,abs(root->val-child->val));
@@ -34,5 +35,25 @@ public:
     int maxAncestorDiff(TreeNode* root) {
         recursiveFunction(root);
         return result;
+    }
+    */
+
+    // Approach two
+    // Optimized approach
+
+    int myFunction(TreeNode *root,int minimum,int maximum){
+        if(root==NULL){
+            return abs(minimum-maximum);
+        }
+        minimum=(minimum>root->val)?root->val:minimum;
+        maximum=(root->val<maximum)?maximum:root->val;
+        int e=myFunction(root->left,minimum,maximum);
+        int f=myFunction(root->right,minimum,maximum);
+        return (e>f)?e:f;
+    }
+    int maxAncestorDiff(TreeNode* root){
+        int minimum=INT_MAX;
+        int maximum=INT_MIN;
+        return myFunction(root,minimum,maximum);
     }
 };
