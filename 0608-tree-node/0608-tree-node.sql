551104e59e991d9011e9b9051959dc08efc79b46
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-01-12 01:21:32 +0530

    Time: 1168 ms (16.41%), Space: 0B (100.00%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -6,4 +6,8 @@
 
 # dusra tarika
 
-SELECT id,CASE WHEN p_id IS NULL THEN 'Root' WHEN id NOT IN(SELECT DISTINCT(p_id) FROM Tree WHERE p_id IS NOT NULL) THEN 'Leaf' ELSE 'Inner' END as type FROM Tree;
+# SELECT id,CASE WHEN p_id IS NULL THEN 'Root' WHEN id NOT IN(SELECT DISTINCT(p_id) FROM Tree WHERE p_id IS NOT NULL) THEN 'Leaf' ELSE 'Inner' END as type FROM Tree;
+
+# Teesra tarika
+
+SELECT id,IF(p_id IS NULL,"Root",IF(id IN(SELECT DISTINCT(p_id) FROM Tree),"Inner","Leaf")) AS type FROM Tree;
