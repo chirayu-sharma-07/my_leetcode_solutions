cb1e215d29b983075835f07a0995439cd68b1b30
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-01-12 19:09:24 +0530

    Time: 4 ms (94.69%), Space: 12.2 MB (68.24%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
+
+        // Approach one
+
+        /*
         int n=tokens.size();
         if(n==1){
             return stoi(tokens[0]);
@@ -24,5 +28,32 @@ public:
             }
         }
         return stoi(st.top());
+        */
+
+        // Approach two
+
+
+        int n=tokens.size();
+        if(n==1) return stoi(tokens[0]);
+        unordered_set<string> us={"+","-","*","/"};
+        stack<int> st;
+        st.push(stoi(tokens[0]));
+        st.push(stoi(tokens[1]));
+        for(int e=0;e<n;e++){
+            if(us.find(tokens[e])==us.end()){
+                st.push(stoi(tokens[e]));
+            }else{
+                int b=st.top();
+                st.pop();
+                int a=st.top();
+                st.pop();
+                if(tokens[e]=="+") a=a+b;
+                else if(tokens[e]=="-") a=a-b;
+                else if(tokens[e]=="*") a=a*b;
+                else a=a/b;
+                st.push(a);
+            }
+        }
+        return st.top();
     }
 };
