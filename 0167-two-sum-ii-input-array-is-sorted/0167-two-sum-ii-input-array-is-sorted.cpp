e3719e3df04ab3aeb5c77f2f9e6d65025acf2d1e
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-01-14 00:30:37 +0530

    Time: 11 ms (52.42%), Space: 15.9 MB (95.18%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
+
+        // Brute force approach
+
+        /*
         unordered_map<int,int> um;
         int n=numbers.size();
         for(int e=0;e<n;e++){
             if(um.find(target-numbers[e])==um.end()){
                 um[numbers[e]]=e;
             }else return {um[target-numbers[e]]+1,e+1};
+        }
+        return {-1,-1};
+        */
+
+        // Optimized approach
+        // Two pointer approach
+
+        int f=numbers.size()-1;
+        int e=0;
+        while(e<f){
+            if(numbers[e]+numbers[f]==target) return {e+1,f+1};
+            else if(numbers[e]+numbers[f]>target) f--;
+            else e++;
         }
         return {-1,-1};
     }
