274811a88d7fbee293eecd3232dbf3161edf2197
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-01-17 03:04:27 +0530

    Time: 15 ms (70.57%), Space: 18 MB (60.15%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
+
+        // Approach one
+        // TC - O(2N)
+        // SC - O(2N)
+
+        /*
         int n=ratings.size();
         vector<int> L2R(n,1);
         vector<int> R2L(n,1);
@@ -18,6 +24,24 @@ public:
         for(int e=0;e<n;e++){
             result+=(L2R[e]>R2L[e])?L2R[e]:R2L[e];
         }
+        return result;
+        */
+
+        // Approach two
+        // TC - O(2N), SC - O(N)
+
+        int n=ratings.size();
+        vector<int> result_count(n,1);
+        for(int e=1;e<n;e++){
+            if(ratings[e]>ratings[e-1]) result_count[e]=result_count[e-1]+1;
+        }
+        for(int e=n-2;e>=0;e--){
+            if(ratings[e]>ratings[e+1]){
+                result_count[e]=max(result_count[e],result_count[e+1]+1);
+            }
+        }
+        int result=0;
+        for(int e=0;e<n;e++) result+=result_count[e];
         return result;
     }
 };
