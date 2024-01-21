faa58f80541c6d141ccd972de07422a9230c1186
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-01-21 13:32:09 +0530

    Time: 1041 ms (5.23%), Space: 17.5 MB (17.22%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -2,12 +2,39 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         // Brute force approach
+        /*
         int m=matrix.size();
         int n=matrix[0].size();
         for(int e=0;e<m;e++){
             for(int f=0;f<n;f++){
                 if(matrix[e][f]==target) return true;
             }
+        }
+        return false;
+        */
+
+        // Second approach
+
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int e=0;
+        int f=0;
+        while(true){
+            if(e<n){
+                for(int i=e;i<n;i++){
+                    if(matrix[e][i]>target) break;
+                    if(matrix[e][i]==target) return true;
+                }
+                e++;
+            }
+            if(f<m){
+                for(int i=f;i<m;i++){
+                    if(matrix[i][f]>target) break;
+                    if(matrix[i][f]==target) return true;
+                }
+                f++;
+            }
+            if(e==n || f==m) break;
         }
         return false;
     }
