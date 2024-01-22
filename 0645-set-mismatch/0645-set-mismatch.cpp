b204d6d2bb275476445391a6930eae37e0824fa7
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-01-22 10:44:31 +0530

    Time: 51 ms (30.50%), Space: 34.4 MB (8.31%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
+        
+        // Approach one
+
+        /*
         int n=nums.size();
         vector<int> occurances(n+1,0);
         for(int e=0;e<n;e++) occurances[nums[e]]++;
@@ -9,6 +13,26 @@ public:
         for(int e=1;e<=n;e++){
             if(occurances[e]==2) a=e;
             if(occurances[e]==0) b=e;
+        }
+        return {a,b};
+        */
+
+        // Approach two
+        // Using set itself
+
+        int n=nums.size();
+        unordered_set<int> mySet;
+        int a=0;
+        int b=0;
+        for(int b=0;b<n;b++){
+            if(mySet.find(nums[b])!=mySet.end()) a=nums[b];
+            else mySet.insert(nums[b]);
+        }
+        b=1;
+        for(;b<=n;b++){
+            if(mySet.find(b)==mySet.end()){
+                break;
+            }
         }
         return {a,b};
     }
