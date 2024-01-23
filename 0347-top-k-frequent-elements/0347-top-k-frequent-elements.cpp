1741e3ea3a4d171ca041e1b7e16a64c07177a4b6
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-01-24 00:55:50 +0530

    Time: 17 ms (26.18%), Space: 17.1 MB (5.04%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        // Approach one
+
+        /*
         unordered_map<int,int> myMap;
         int n=nums.size();
         vector<int> result;
@@ -14,6 +18,25 @@ public:
         while(k--){
             result.push_back(pq.top().second);
             pq.pop();
+        }
+        return result;
+        */
+
+        // Approach two
+
+        unordered_map<int,int> myMap;
+        vector<int> result;
+        for(auto &e:nums){
+            myMap[e]++;
+        }
+        vector<pair<int,int>> vec;
+        for(auto &e:myMap){
+            vec.push_back(make_pair(e.second,e.first));
+        }
+        sort(begin(vec),end(vec));
+        int e=vec.size()-1;
+        while(k--){
+            result.push_back(vec[e--].second);
         }
         return result;
     }
