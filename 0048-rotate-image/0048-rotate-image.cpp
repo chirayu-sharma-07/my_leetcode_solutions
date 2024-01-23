69658a6cf2d18ce9719749a1dbcee3f9a8bb0dc8
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-01-23 23:47:11 +0530

    Time: 4 ms (39.79%), Space: 8.6 MB (9.76%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+
+        // Old code
+
+        /*
         int sizeOfMatrix=matrix.size();
         for(int e=0;e<sizeOfMatrix-1;e++){
             for(int f=e+1;f<sizeOfMatrix;f++){
@@ -14,6 +18,27 @@ public:
                 int h=matrix[e][f];
                 matrix[e][f]=matrix[e][g];
                 matrix[e][g]=h;
+            }
+        }
+        */
+
+
+        // Once again
+
+        int n=matrix.size();
+        for(int e=0;e<n;e++){
+            for(int f=e;f<n;f++){
+                if(e==f) continue;
+                int g=matrix[e][f];
+                matrix[e][f]=matrix[f][e];
+                matrix[f][e]=g;
+            }
+        }
+        for(int e=0;e<n;e++){
+            for(int f=0,g=n-1;f<g;f++,g--){
+                int temp=matrix[e][f];
+                matrix[e][f]=matrix[e][g];
+                matrix[e][g]=temp;
             }
         }
     }
