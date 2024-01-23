b4165998dab692a676b9d07feb4072b481bce5f4
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-01-23 13:22:59 +0530

    Time: 15 ms (76.32%), Space: 17.2 MB (65.36%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+
+    // Approach one
+    // Wrong approach because i used set, Instead i should i use temporary string
+
+    /*
+    int N=0;
+    bool check(string s,unordered_set<char> &mySet){
+        string temp_string="";
+        int n=s.size();
+        for(int e=0;e<n;e++){
+            if(mySet.find(s[e])!=mySet.end()) return false;
+            else{
+                temp_string+=s[e];
+            }
+        }
+        for(int e=0;e<n;e++){
+            mySet.insert(temp_string[e]);
+        }
+        return true;
+    }
+    int myFunction(vector<string> &arr,int e,unordered_set<char> &mySet){
+        if(e>=N) return mySet.size();
+        int take=0;
+        int skip=0;
+        if(check(arr[e],mySet)) take=myFunction(arr,e+1,mySet);
+        skip=myFunction(arr,e+1,mySet);
+        return max(take,skip);
+    }
+    int maxLength(vector<string>& arr) {
+        unordered_set<char> mySet;
+        N=arr.size();
+        return myFunction(arr,0,mySet);
+    }
+    */
+
+    // Approach two
+
+    int N=0;
+    bool check(string s1,string s2){
+        int freq[26]={0};
+        for(char &ch:s1){
+            if(freq[ch-'a']>0) return false;
+            freq[ch-'a']++;
+        }
+        for(char &ch:s2){
+            if(freq[ch-'a']>0) return false;
+        }
+        return true;
+    }
+    int myFunction(vector<string> &arr,int e,string temp_string){
+        if(e>=N) return temp_string.size();
+        int take=0;
+        int skip=0;
+        if(check(arr[e],temp_string)) take=myFunction(arr,e+1,temp_string+arr[e]);
+        skip=myFunction(arr,e+1,temp_string);
+        return max(take,skip);
+    }
+    int maxLength(vector<string> &arr){
+        N=arr.size();
+        return myFunction(arr,0,"");
+    }
+};
