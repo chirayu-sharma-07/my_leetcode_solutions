7690152cbb92ad7cc97ecee8cfac0aec4e183353
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-01-26 15:11:57 +0530

    Time: 18 ms (93.58%), Space: 25.6 MB (10.18%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -68,6 +68,7 @@ public:
 
     // Once more for practice purpose
 
+    /*
     string s,t;
     int m,n;
     int memo[1001][1001];
@@ -84,5 +85,19 @@ public:
         t=s2;
         memset(memo,-1,sizeof(memo));
         return recursiveFunction(0,0);
+    }
+    */
+
+    int longestCommonSubsequence(string s1,string s2){
+        int m=s1.size();
+        int n=s2.size();
+        vector<vector<int>> bot_up(m+1,vector<int>(n+1));
+        for(int e=1;e<=m;e++){
+            for(int f=1;f<=n;f++){
+                if(s1[e-1]==s2[f-1]) bot_up[e][f]=1+bot_up[e-1][f-1];
+                else bot_up[e][f]=max(bot_up[e-1][f],bot_up[e][f-1]);
+            }
+        }
+        return bot_up[m][n];
     }
 };
