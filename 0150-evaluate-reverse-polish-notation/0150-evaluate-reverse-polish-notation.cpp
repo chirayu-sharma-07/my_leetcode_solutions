1a21b14e91a14467ad496a43f0e1ebe52389aefd
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-01-30 10:20:04 +0530

    Time: 3 ms (98.68%), Space: 15.4 MB (10.51%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
+    /*
     int evalRPN(vector<string>& tokens) {
 
         // Approach one
 
-        /*
+        
         int n=tokens.size();
         if(n==1){
             return stoi(tokens[0]);
@@ -28,7 +29,7 @@ public:
             }
         }
         return stoi(st.top());
-        */
+        
 
         // Approach two
 
@@ -52,6 +53,30 @@ public:
                 else if(tokens[e]=="*") a=a*b;
                 else a=a/b;
                 st.push(a);
+            }
+        }
+        return st.top();
+    }
+    */
+
+
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        int n=tokens.size();
+        if(n==1) return stoi(tokens[0]);
+        for(int e=0;e<n;e++){
+            if(tokens[e]=="+" || tokens[e]=="-" || tokens[e]=="*" || tokens[e]=="/"){
+                int b=st.top();
+                st.pop();
+                int a=st.top();
+                st.pop();
+                if(tokens[e]=="+") a=a+b;
+                else if(tokens[e]=="-") a=a-b;
+                else if(tokens[e]=="*") a=a*b;
+                else a=a/b;
+                st.push(a);
+            }else{
+                st.push(stoi(tokens[e]));
             }
         }
         return st.top();
