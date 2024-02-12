1b447496394ecacbc7d94f8ac2457d183698c594
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-02-12 20:14:08 +0530

    Time: 7 ms (97.54%), Space: 22.9 MB (8.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+
+        // Brute force approach
+
+        /*
         int n=nums.size();
         unordered_map<int,int> myMap;
         for(int e=0;e<n;e++){
@@ -9,6 +13,23 @@ public:
         n/=2;
         for(auto &e:myMap){
             if(e.second>n) return e.first;
+        }
+        return -1;
+        */
+
+        // Optimized approach
+
+        sort(begin(nums),end(nums));
+        int n=nums.size();
+        for(int e=0;e<n;e++){
+            int num=nums[e];
+            int f=e;
+            while(f<n && nums[f]==num) f++;
+            f--;
+            if((f-e)+1>n/2) return num;
+            else{
+                e=f;
+            }
         }
         return -1;
     }
