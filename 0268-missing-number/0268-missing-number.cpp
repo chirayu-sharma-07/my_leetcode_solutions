225f20d809295246165da8727c82dd8087e50cee
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-02-20 16:17:45 +0530

    Time: 18 ms (33.43%), Space: 21.1 MB (5.76%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -4,6 +4,7 @@ public:
 
         // Noob approach
         
+        /*
         unordered_set<int> mySet;
         int n=nums.size();
         for(int e=0;e<n;e++){
@@ -14,5 +15,16 @@ public:
             if(mySet.find(n)==mySet.end()) return n;
         }
         return 0;
+        */
+        
+        // Second approach
+        
+        sort(begin(nums),end(nums));
+        if(nums[0]!=0) return 0;
+        int n=nums.size();
+        for(int e=0;e<n-1;e++){
+            if((nums[e]+1)!=nums[e+1]) return nums[e]+1;
+        }
+        return n;
     }
 };
