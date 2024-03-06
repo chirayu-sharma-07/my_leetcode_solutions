6fcdc98f6dfda039dd4f20aa8ddc49f54478f807
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-03-06 19:30:35 +0530

    Time: 12 ms (26.14%), Space: 10.6 MB (26.02%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,6 +9,10 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        
+        // First approach
+
+        /*
         if(!head) return false;
         if(!head->next) return false;
         if(head->next==head) return true;
@@ -20,6 +24,18 @@ public:
             if(slow==fast) return true;
             slow=slow->next;
             fast=fast->next->next;
+        }
+        return false;
+        */
+
+        /* Second approach (Same as first approach but this approach doesn't require to apply validations for empty,single nodes) */
+
+        ListNode *fast=head;
+        ListNode *slow=fast;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast) return true;
         }
         return false;
     }
