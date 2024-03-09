20c8d78eab05c071b254be891646b23ef83173a4
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-03-09 08:06:02 +0530

    Time: 148 ms (10.11%), Space: 69.2 MB (14.66%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,12 +1,34 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
+
+        // First approach
+
+        /*
         int n=nums1.size();
         int m=nums2.size();
         for(int e=0,f=0;e<n && f<m;){
             if(nums1[e]==nums2[f]) return nums1[e];
             if(nums1[e]<nums2[f]) e++;
             else f++;
+        }
+        return -1;
+        */
+
+        // Second approach
+
+        set<int> mySet;
+        int n=nums1.size();
+        int m=nums2.size();
+        for(int e=0;e<n;){
+            int num=nums1[e];
+            mySet.insert(num);
+            while(e<n && nums1[e]==num) e++;
+        }
+        for(int e=0;e<m;){
+            int num=nums2[e];
+            if(mySet.find(num)!=mySet.end()) return num;
+            while(e<m && nums2[e]==num) e++;
         }
         return -1;
     }
