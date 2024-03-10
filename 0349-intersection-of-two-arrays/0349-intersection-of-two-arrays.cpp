b1d5481fe33d0a69b9b58187e1354566a1a736e3
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-03-10 12:37:04 +0530

    Time: 3 ms (83.79%), Space: 12.7 MB (41.99%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+
+        // First approach
+
+        /*
         set<int> mySet;
         vector<int> result;
         int n=nums1.size();
@@ -15,6 +19,39 @@ public:
                 result.push_back(nums2[e]);
                 mySet.erase(nums2[e]);
             }
+        }
+        return result;
+        */
+
+        // Second approach
+
+        vector<int> first_temp;
+        vector<int> second_temp;
+        int n=nums1.size();
+        int m=nums2.size();
+        sort(begin(nums1),end(nums1));
+        sort(begin(nums2),end(nums2));
+        for(int e=0;e<n;){
+            int num=nums1[e];
+            while(e<n && nums1[e]==num) e++;
+            first_temp.push_back(num);
+        }
+        for(int e=0;e<m;){
+            int num=nums2[e];
+            while(e<m && nums2[e]==num) e++;
+            second_temp.push_back(num);
+        }
+        vector<int> result;
+        n=first_temp.size();
+        m=second_temp.size();
+        for(int e=0,f=0;e<n && f<m;){
+            if(first_temp[e]==second_temp[f]){
+                result.push_back(second_temp[f]);
+                e++;
+                f++;
+            }else if(first_temp[e]<second_temp[f]){
+                e++;
+            }else f++;
         }
         return result;
     }
