15ce83dfc87c7ad2b470b27dce228f867e36d7bc
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-03-20 08:44:11 +0530

    Time: 3 ms (42.92%), Space: 7.1 MB (97.62%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int pivotInteger(int n) {
         int e=1;
-        int f=n;
         int prefix=1;
         int postfix=n;
-        while(e<f){
+        while(e<n){
             if(prefix<postfix){
                 e++;
                 prefix+=e;
             }else{
-                f--;
-                postfix+=f;
+                n--;
+                postfix+=n;
             }
         }
         return (prefix==postfix)?e:-1;
