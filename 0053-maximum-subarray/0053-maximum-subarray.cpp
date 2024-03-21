4b83f5ed48c1bd4a77f74e9789b661b519cfc103
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-03-21 08:29:28 +0530

    Time: 171 ms (5.54%), Space: 70.3 MB (30.10%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+
+        // First approach
+
+        /*
         int result=INT_MIN;
         int sub_result=0;
         int n=nums.size();
@@ -8,6 +12,29 @@ public:
             sub_result+=nums[e];
             if(result<sub_result) result=sub_result;
             if(sub_result<0) sub_result=0;
+        }
+        return result;
+        */
+
+        // Follow-up question (Print the elements of array having the largest sum)
+
+        int n=nums.size();
+        int result=INT_MIN;
+        int sub_result=0;
+        int startIndex=-1;
+        int endIndex=-1;
+        for(int e=0;e<n;e++){
+            if(sub_result==0) startIndex=e;
+            sub_result+=nums[e];
+            if(result<sub_result){
+                result=sub_result;
+                endIndex=e;
+            }
+            if(sub_result<0) sub_result=0;
+        }
+        while(startIndex<=endIndex){
+            cout<<nums[startIndex]<<endl;
+            startIndex++;
         }
         return result;
     }
