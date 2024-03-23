4e357e7aebf4bef4e9ba68f8c96e9f2c67b0716e
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-03-23 10:18:08 +0530

    Time: 26 ms (46.48%), Space: 21.3 MB (69.02%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -10,10 +10,24 @@
  */
 class Solution {
 public:
+    ListNode* reverse_linked_list(ListNode *head){
+        if(!head->next) return head;
+        ListNode *p1=NULL;
+        ListNode *p2=head->next;
+        while(p2){
+            head->next=p1;
+            p1=head;
+            head=p2;
+            p2=p2->next;
+        }
+        head->next=p1;
+        return head;
+    }
     void reorderList(ListNode* head) {
         
         // First approach (Using Stack)
 
+        /*
         if(!head->next || !head->next->next) return;
         ListNode *myNode=new ListNode(7);
         myNode->next=head;
@@ -36,6 +50,28 @@ public:
             slow->next=st.top();
             st.pop();
             slow->next->next=fast;
+            slow=slow->next->next;
+        }
+        return;
+        */
+
+        // Second approach (By reversing right half of the linked list)
+
+        if(!head->next || !head->next->next) return;
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(fast->next && fast->next->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        fast=reverse_linked_list(slow->next);
+        slow->next=NULL;
+        slow=head;
+        while(fast){
+            ListNode *temp=slow->next;
+            slow->next=fast;
+            fast=fast->next;
+            slow->next->next=temp;
             slow=slow->next->next;
         }
         return;
