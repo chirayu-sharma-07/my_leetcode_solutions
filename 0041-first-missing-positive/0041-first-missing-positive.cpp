ed97cb06586f7b2420b630f79ecf98bcda99537f
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-03-26 12:44:56 +0530

    Time: 52 ms (28.16%), Space: 43.7 MB (55.21%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+        
+        // First approach
+
+        /*
         unordered_set<int> mySet;
         for(int &e:nums){
             if(e>0){
@@ -15,5 +19,19 @@ public:
             e++;
         }
         return 1;
+        */
+
+        // Second approach (Taking O(N) extra space)
+
+        int n=nums.size();
+        vector<bool> vec(n+1,false);
+        for(int &e:nums){
+            if(e<1 || e>n) continue;
+            vec[e]=true;
+        }
+        for(int e=1;e<=n;e++){
+            if(!vec[e]) return e;
+        }
+        return n+1;
     }
 };
