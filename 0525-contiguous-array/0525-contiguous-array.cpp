69e13019bb07e20f14e183992626d76334b2393d
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-03-27 00:05:23 +0530

    Time: 76 ms (82.56%), Space: 87.8 MB (50.94%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -4,8 +4,8 @@ public:
         int n=nums.size();
         unordered_map<int,int> myMap;
         myMap[0]=-1;
-        int result=0;
         int sum=0;
+        int result=0;
         for(int e=0;e<n;e++){
             sum+=(nums[e]==1)?1:-1;
             if(myMap.find(sum)!=myMap.end()){
