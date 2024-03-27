f2b6892018a9fa96668c2edc8a675a5874c35e95
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-03-27 08:57:26 +0530

    Time: 54 ms (74.10%), Space: 63.6 MB (90.38%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+
+        // Brute force approach
+
+        /*
         int result=0;
         int n=nums.size();
         for(int e=0;e<n;e++){
@@ -11,6 +15,23 @@ public:
                 if(product<k) result++;
                 else break;
             }
+        }
+        return result;
+        */
+
+        // Sliding window technique
+
+        int n=nums.size();
+        if(k<=1) return 0;
+        int result=0;
+        for(int e=0,f=0,product=1;f<n;){
+            product*=nums[f];
+            while(product>=k){
+                product/=nums[e];
+                e++;
+            }
+            result+=(f-e+1);
+            f++;
         }
         return result;
     }
