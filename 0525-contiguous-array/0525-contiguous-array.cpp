d9c6c615df150ee8146925586b31d29eb229716e
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-03-27 19:01:57 +0530

    Time: 102 ms (16.17%), Space: 87.8 MB (20.81%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
         int n=nums.size();
+        int result=0;
         unordered_map<int,int> myMap;
         myMap[0]=-1;
         int sum=0;
-        int result=0;
         for(int e=0;e<n;e++){
-            sum+=(nums[e]==1)?1:-1;
+            sum+=(nums[e]==0)?-1:1;
             if(myMap.find(sum)!=myMap.end()){
                 result=max(result,e-myMap[sum]);
             }else myMap[sum]=e;
