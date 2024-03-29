52fab2cdcd1b7865050d0bcd82142a3a5d66db78
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-03-30 00:46:55 +0530

    Time: 60 ms (42.86%), Space: 45.5 MB (44.96%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,30 +1,6 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-
-        // Wrong approach
-
-        /*
-        sort(begin(people),end(people));
-        int result=0;
-        int n=people.size();
-        for(int e=0;e<n;e++){
-            if(people[e]==limit){
-                result++;
-                continue;
-            }
-            if(e<n-1 && people[e]+people[e+1]<=limit){
-                result++;
-                e++;
-                continue;
-            }
-            result++;
-        }
-        return result;
-        */
-
-        // Second approach
-
         sort(begin(people),end(people));
         int e=0;
         int f=people.size()-1;
@@ -33,8 +9,8 @@ public:
             if(people[e]+people[f]<=limit){
                 e++;
             }
-            f--;
             result++;
+            f--;
         }
         return result;
     }
