325ef57a801236fd7e541f693a109b45cf1b0f69
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-04-10 09:38:31 +0530

    Time: 7 ms (18.53%), Space: 11 MB (34.66%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
+
+        // First approach
+
+        /*
         sort(begin(deck),end(deck));
         int n=deck.size();
         bool flag=false;
@@ -16,6 +20,25 @@ public:
                 flag=!flag;
             }
             f=(f+1)%n;
+        }
+        return result;
+        */
+
+        // Second approach
+
+        sort(begin(deck),end(deck));
+        int n=deck.size();
+        queue<int> q;
+        vector<int> result(n);
+        for(int e=0;e<n;e++) q.push(e);
+        for(int e=0;e<n;e++){
+            int index=q.front();
+            q.pop();
+            result[index]=deck[e];
+            if(!q.empty()){
+                q.push(q.front());
+                q.pop();
+            }
         }
         return result;
     }
