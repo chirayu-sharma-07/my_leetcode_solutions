636b4d958de3e894a3c65ee142374dd81732f56c
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-04-19 15:52:44 +0530

    Time: 28 ms (62.85%), Space: 15.9 MB (62.14%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-    int m=0;
-    int n=0;
-    void recursiveFunction(int e,int f,vector<vector<char>>& grid,vector<vector<bool>> &flag_matrix){
-        flag_matrix[e][f]=true;
-        if(e>0 && grid[e-1][f]=='1' && flag_matrix[e-1][f]==false){
-            recursiveFunction(e-1,f,grid,flag_matrix);
-        }
-        if(f>0 && grid[e][f-1]=='1' && flag_matrix[e][f-1]==false){
-            recursiveFunction(e,f-1,grid,flag_matrix);
-        }
-        if(e<m-1 && grid[e+1][f]=='1' && flag_matrix[e+1][f]==false){
-            recursiveFunction(e+1,f,grid,flag_matrix);
-        }
-        if(f<n-1 && grid[e][f+1]=='1' && flag_matrix[e][f+1]==false){
-            recursiveFunction(e,f+1,grid,flag_matrix);
+    int m,n;
+    void cover_whole_island(int e,int f,vector<vector<char>> &grid,vector<vector<bool>> &visited){
+        if(grid[e][f]=='1' && !visited[e][f]){
+            visited[e][f]=!visited[e][f];
+            if(e+1<m) cover_whole_island(e+1,f,grid,visited);
+            if(f+1<n) cover_whole_island(e,f+1,grid,visited);
+            if(e-1>=0) cover_whole_island(e-1,f,grid,visited);
+            if(f-1>=0) cover_whole_island(e,f-1,grid,visited);
         }
     }
     int numIslands(vector<vector<char>>& grid) {
-       m=grid.size();
-       n=grid[0].size();
-       vector<vector<bool>> flag_matrix(m,vector<bool>(n,false));
-       int result=0;
-       for(int e=0;e<m;e++){
-           for(int f=0;f<n;f++){
-               if(flag_matrix[e][f]==false && grid[e][f]=='1'){
-                   result++;
-                   recursiveFunction(e,f,grid,flag_matrix);
-               }
-           }
-       }
-       return result;
+        m=grid.size();
+        n=grid[0].size();
+        vector<vector<bool>> visited(m,vector<bool>(n,false));
+        int result=0;
+        for(int e=0;e<m;e++){
+            for(int f=0;f<n;f++){
+                if(grid[e][f]=='1' && !visited[e][f]){
+                    result++;
+                    cover_whole_island(e,f,grid,visited);
+                }
+            }
+        }
+        return result;
     }
 };
