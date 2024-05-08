304a6fa7d2eb19157efd96a9b22933defcc3a017
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-05-08 08:20:07 +0530

    Time: 10 ms (51.87%), Space: 13.2 MB (99.63%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
+
+        // First code
+
+        /*
         int n=score.size();
         vector<string> result(n);
         vector<pair<int,int>> vec;
@@ -16,6 +20,29 @@ public:
             else if(e==1) result[index]="Silver Medal";
             else if(e==2) result[index]="Bronze Medal";
             else result[index]=to_string(e+1);
+        }
+        return result;
+        */
+
+        // Second code
+
+        int n=score.size();
+        vector<string> result(n);
+        vector<pair<int,int>> vec(n);
+        for(int e=0;e<n;e++){
+            vec[e]=make_pair(score[e],e);
+        }
+        sort(begin(vec),end(vec),[](const pair<int,int> &e,const pair<int,int> &f){
+            return e.first>f.first;
+        });
+        result[vec[0].second]="Gold Medal";
+        if(n>1){
+            result[vec[1].second]="Silver Medal";
+        }else return result;
+        if(n>2) result[vec[2].second]="Bronze Medal";
+        else return result;
+        for(int e=3;e<n;e++){
+            result[vec[e].second]=to_string(e+1);
         }
         return result;
     }
