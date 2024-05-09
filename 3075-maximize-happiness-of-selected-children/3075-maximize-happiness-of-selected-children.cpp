863f870e242cb4824c95116042d4185bf809649b
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-05-10 00:20:29 +0530

    Time: 154 ms (78.85%), Space: 107.1 MB (89.28%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
+
+        // First approach (Unoptimized)
+
+        /*
         long long result=0;
         priority_queue<int> pq;
         for(int &e:happiness) pq.push(e);
@@ -10,6 +14,18 @@ public:
             else break;
             pq.pop();
             count++;
+        }
+        return result;
+        */
+
+        // Abhi optimize karke deta hoon
+
+        sort(begin(happiness),end(happiness),greater<int>());
+        long long result=0;
+        int count=0;
+        for(int e=0;k--;count++,e++){
+            if(happiness[e]-count>0) result+=happiness[e]-count;
+            else break;
         }
         return result;
     }
