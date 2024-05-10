558fe85a70dc8e3b44f2affd30ab3fffff05ff99
Chirayu Sharma <110329778+chirayu-sharma-07@users.noreply.github.com>
2024-05-10 16:19:35 +0530

    Time: 1152 ms (15.48%), Space: 257.3 MB (11.03%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+
+        // First approach
+
+        /*
         vector<pair<double,pair<int,int>>> vec;
         int n=arr.size();
         for(int e=0;e<n;e++){
@@ -10,5 +14,19 @@ public:
         }
         sort(begin(vec),end(vec));
         return {vec[k-1].second.first,vec[k-1].second.second};
+        */
+
+        // Second approach
+
+        priority_queue<vector<double>> pq;
+        int n=arr.size();
+        for(int e=0;e<n;e++){
+            for(int f=e+1;f<n;f++){
+                pq.push({(double)arr[e]/arr[f],(double)e,(double)f});
+                if(pq.size()>k) pq.pop();
+            }
+        }
+        vector<double> vec=pq.top();
+        return {arr[vec[1]],arr[vec[2]]};
     }
 };
